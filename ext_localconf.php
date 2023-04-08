88fcb57a39cf1e7e803dcbd4f1d2d81675241563
Alexander Grein <alexander.grein@gmail.com>
2023-04-08 23:18:44 +0200

    Add basic stuff for upcoming TYPO3 12 compatibility

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 (function () {
     $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects']['TYPOSCRIPT_RENDERING'] = \Helhum\TyposcriptRendering\ContentObject\TypoScriptRenderingContentObject::class;
